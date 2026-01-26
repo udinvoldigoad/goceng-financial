07b8db95b24c9b2e260e0b0f01493c60bdedd68a
Ridhoms05 <ridhomsaputra328@gmail.com>
2026-01-26 19:29:02 +0700

    chore: Update PWA asset and icon references from `goceng.png` to `logo.png`.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['goceng.png'],
+      includeAssets: ['logo.png'],
       manifest: {
         name: 'Goceng - Finance Tracker',
         short_name: 'Goceng',
@@ -23,17 +23,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'goceng.png',
+            src: 'logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'goceng.png',
+            src: 'logo.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'goceng.png',
+            src: 'logo.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
